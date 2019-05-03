5af559c0abab422816d43b663da490881775773a
unknown <andersondenise406@gmail.com>
2019-05-02 21:33:02 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route,} from "react-router-dom";
+//Import bootstrap, font-awsome, etc.
+
+import "./App.css";
+
+import Landing from "./components/pages/Landing";
+import GameApp from "./components/pages/GameApp";
+import Quotes from "./components/pages/Quotes";
+import Recipe from "./components/pages/Recipe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+       <Router>
+           <div>
+               <Route exact path="/" components={Landing} />
+                <Route path="/GameApp" components={GameApp} /> 
+                <Route path="/Quotes" components={Quotes} /> 
+               <Route path="/Recipe" components={Recipe} />
+           </div>
+       </Router>
+   )
 }
 
 export default App;
