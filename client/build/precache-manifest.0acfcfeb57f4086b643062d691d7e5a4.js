791e9a36467b1f95aadc56e7cd7b3f01201a8c92
Kate <kef12@comcast.net>
2019-05-04 12:52:06 -0600

    initial commit

---
+++ client/build/precache-manifest.0acfcfeb57f4086b643062d691d7e5a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab494ff5635fcd18f05a54d2b397224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8207b332d4c14c7f77f",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a97f73d495404001a9ec",
+    "url": "/static/css/3.03de18cd.chunk.css"
+  },
+  {
+    "revision": "983ae9407d9a0e943082",
+    "url": "/static/css/4.d81a63fe.chunk.css"
+  },
+  {
+    "revision": "ad4a4163aa3efad7ba8c",
+    "url": "/static/css/main.5e296057.chunk.css"
+  },
+  {
+    "revision": "a8207b332d4c14c7f77f",
+    "url": "/static/js/2.72459ab0.chunk.js"
+  },
+  {
+    "revision": "a97f73d495404001a9ec",
+    "url": "/static/js/3.7fecbd93.chunk.js"
+  },
+  {
+    "revision": "983ae9407d9a0e943082",
+    "url": "/static/js/4.6eb5f1ee.chunk.js"
+  },
+  {
+    "revision": "ad4a4163aa3efad7ba8c",
+    "url": "/static/js/main.95c6a726.chunk.js"
+  },
+  {
+    "revision": "1639002facd445c530bc",
+    "url": "/static/js/runtime~main.fb5ec2c0.js"
+  },
+  {
+    "revision": "c3a398cf13d42db3c987b38f6aa04811",
+    "url": "/static/media/quoteBackground.c3a398cf.jpg"
+  }
+]);
